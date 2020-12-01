f4ba4734a9d5e744d77476c109412777dea334d6
Logan Thirion <thirionlogan@gmail.com>
2020-12-01 12:11:22 -0600

    working login page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-
+function navigateToPage(page) {
+  const pages = ['loginPage', 'browsePage'];
+  pages
+    .filter((value) => {
+      return page !== value;
+    })
+    .forEach((elementId) => {
+      document.getElementById(elementId).classList.add('hide');
+    });
+  var page = document.getElementById(page);
+  page.classList.remove('hide');
+}
