d070cc64260a9dfe91a59997bc474a1256c34dba
Logan Thirion <thirionlogan@gmail.com>
2020-12-04 19:52:49 -0600

    alphabetical sort

--- scripts.js
+++ scripts.js
@@ -262,26 +262,6 @@ function navigateToMoviePage(movie) {
   navigateToPage('movieDetailsPage');
 }
 
-function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
 function loadResources() {
   const searchText = document.getElementById('search').value;
   var browsePage = document.getElementById('browsePage');
@@ -296,6 +276,9 @@ function loadResources() {
           movie.description.toLowerCase().includes(searchText.toLowerCase())
         );
       }
+    })
+    .sort((movieA, movieB) => {
+      return movieA.name.toLowerCase() > movieB.name.toLowerCase() ? 1 : -1;
     })
     .forEach((movie) => {
       var movieElement = document.createElement('img');
